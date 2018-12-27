50200c592be9bac9b55c8a5aacccf08d55ea841a
pedroglp <pedrogabriellancelloti@poli.ufrj.br>
2018-12-27 17:57:59 -0200

    Add singup route

---
+++ api/controllers/singup.js
@@ -0,0 +1,17 @@
+module.exports = async function(request, reply) {
+    const {username, password, email} = request.body
+    const users = this.mongo.db.collection('users')
+    
+    // checks if email/username is already in use
+    const user = await users.findOne({$or:[{email}, {username}]})
+    
+    if(!!user) {
+        return reply.code(409).send({msg: `${user.email === email ? 
+            'E-mail already in use.' 
+            : 'Username already in use.'}`})
+    }
+
+    users.insert({username, password, email})
+
+    return reply.code(202).send({ msg: 'Successfully registred.', username, email })
+}
