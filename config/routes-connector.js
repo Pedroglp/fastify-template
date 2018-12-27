30282183667d9560662e39a045d5300af40b44f9
pedroglp <pedrogabriellancelloti@poli.ufrj.br>
2018-12-27 09:34:13 -0200

    Remove database from routes connector

--- config/routes-connector.js
+++ config/routes-connector.js
@@ -1,10 +1,7 @@
 const fastifyPlugin = require('fastify-plugin')
 
 async function routesConnector(fastify, routes) {
-  const database = fastify.mongo.db('db')
-
   routes.forEach(route => {
-    route.handler.database = database;
     fastify.route(route)
   });
 }
