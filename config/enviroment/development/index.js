2eb978706b8afc5724886a843743dd389fbed366
pedroglp <pedrogabriellancelloti@poli.ufrj.br>
2018-12-27 13:08:21 -0200

    Fix app and mongo docker integration, change default password

--- config/enviroment/development/index.js
+++ config/enviroment/development/index.js
@@ -1,7 +1,7 @@
 module.exports = {
   logger: true,
   db: {
-    url: 'mongodb://fastify:FaF#2437@localhost:27017/test',
+    url: 'mongodb://fastify:password@mongo:27017/admin',
     forceClose: true
   }
 }
