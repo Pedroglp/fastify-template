e4f61781b7db9ad93bfa6e0547c8271e557fac93
pedroglp <pedrogabriellancelloti@poli.ufrj.br>
2018-12-27 17:59:25 -0200

    Fix ip for docker

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { db, logger } = require('./config/enviroment')
+const { db, logger, jwt } = require('./config/enviroment')
 const fastify = require('fastify')({logger})
 const routes = require('./api/routes.js')
 
@@ -6,10 +6,12 @@ fastify.register(require('fastify-mongodb'), {...db})
 
 fastify.register(require('./config/routes-connector.js'), routes)
 
+fastify.register(require('fastify-jwt'), {...jwt})
+
 
 const start = async () => {
   try {
-    await fastify.listen(3000)
+    await fastify.listen(3000, '0.0.0.0')
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
