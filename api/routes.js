cb2ce07e457c8342b669cc62b6750a0c346c6777
pedroglp <pedrogabriellancelloti@poli.ufrj.br>
2018-12-27 17:58:07 -0200

    Add singup route

--- api/routes.js
+++ api/routes.js
@@ -1,8 +1,10 @@
 const helloWorldController = require('./controllers/helloWorld.js')
 const myRouteController = require('./controllers/my-route.js')
+const signupController = require('./controllers/singup.js')
 
 // https://www.fastify.io/docs/latest/Routes/
 module.exports = [
   { method: 'GET', path: '/', handler: helloWorldController},
-  { method: 'GET', path: '/test/:id', handler: myRouteController}
+  { method: 'GET', path: '/test/:id', handler: myRouteController},
+  { method: 'POST', path: '/signup', handler: signupController}
 ]
