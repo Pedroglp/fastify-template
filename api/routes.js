63b13618d80e5eb3afedb9660bda4f17fa88d1eb
Pedro Gabriel <pedrogabriellancelloti@poli.ufrj.br>
2018-12-28 14:13:35 -0200

    Add users route

--- api/routes.js
+++ api/routes.js
@@ -1,8 +1,14 @@
+// HANDLERS
 const signupController = require('./controllers/singup.js')
 const authController = require('./controllers/auth.js')
+const userController = require('./controllers/users.js')
+
+// HOOKS
+const checkAuth = require('./hooks/checkAuth.js')
 
 // https://www.fastify.io/docs/latest/Routes/
 module.exports = [
   { method: 'POST', path: '/signup', handler: signupController},
-  { method: 'POST', path: '/auth', handler: authController}
+  { method: 'POST', path: '/auth', handler: authController},
+  { method: 'GET', path: '/users', handler: userController, preValidation: checkAuth}
 ]
