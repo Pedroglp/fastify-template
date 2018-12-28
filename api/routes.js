f765c440b0ae8df08427d8daf20086db53dff936
Pedro Gabriel <pedrogabriellancelloti@poli.ufrj.br>
2018-12-28 18:09:11 -0200

    Resolve merge conflicts

--- api/routes.js
+++ api/routes.js
@@ -3,23 +3,13 @@ const helloWorldHandler = require('./handlers/helloWorld.js')
 const myRouteHandler = require('./handlers/my-route.js')
 
 // SCHEMAS
-<<<<<<< Updated upstream
 /** const mySchema = require('./schemas/mySchema.js) */
-=======
-const signupSchema = require('./schemas/signup.js')
->>>>>>> Stashed changes
 
 // HOOKS
 /** const myHook = require('./hooks/myHook.js) */
 
 // https://www.fastify.io/docs/latest/Routes/
 module.exports = [
-<<<<<<< Updated upstream
   { method: 'GET', path: '/', handler: helloWorldHandler},
   { method: 'GET', path: '/test/:id', handler: myRouteHandler}
-=======
-  { method: 'POST', path: '/signup', handler: signupHandler, schema: signupSchema},
-  { method: 'POST', path: '/auth', handler: authHandler},
-  { method: 'GET', path: '/users', handler: userHandler, beforeHandler: checkAuth}
->>>>>>> Stashed changes
 ]
