f228102b4d5273c75eb9955bc6b75a14f35cbdad
Pedro Gabriel <pedrogabriellancelloti@poli.ufrj.br>
2018-12-28 07:32:24 -0200

    Rearrange filesystem

--- config/env/index.js
+++ config/env/index.js
@@ -1,11 +1,11 @@
 if(process.env.NODE_ENV === 'development') {
-  module.exports = require('./development')
+  module.exports = require('./development.js')
 }
 
 if(process.env.NODE_ENV === 'production') {
-  module.exports = require('.production')
+  module.exports = require('./production.js')
 }
 
 if(process.env.NODE_ENV === 'stage') {
-  module.exports = require('./stage')
+  module.exports = require('./stage.js')
 }
