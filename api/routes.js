cd816fdd3ee2d46e3cabe7a1ea0dc2f4b56bdc2b
Pedroglp <pedrogabriellancelloti@poli.ufrj.br>
2018-12-31 09:27:02 -0200

    Add new hook for role based actions

--- api/routes.js
+++ api/routes.js
@@ -8,10 +8,11 @@ const signupSchema = require('./schemas/signup.js')
 
 // HOOKS
 const checkAuth = require('./hooks/checkAuth.js')
+const checkRole = require('./hooks/checkRole.js')
 
 // https://www.fastify.io/docs/latest/Routes/
 module.exports = [
   { method: 'POST', path: '/signup', handler: signupHandler, schema: signupSchema},
   { method: 'POST', path: '/auth', handler: authHandler},
-  { method: 'GET', path: '/users', handler: userHandler, beforeHandler: checkAuth}
+  { method: 'GET', path: '/users', handler: userHandler, beforeHandler: [checkAuth, checkRole]}
 ]
