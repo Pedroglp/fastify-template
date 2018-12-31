386e4f9b1dfd138d00141c3b9a0026626ecc5401
Pedroglp <pedrogabriellancelloti@poli.ufrj.br>
2018-12-31 09:24:31 -0200

    Add comments explaining how to get user from request

--- api/handlers/users.js
+++ api/handlers/users.js
@@ -1,5 +1,8 @@
 // TODO: use schemas for response
 module.exports = async function(request, reply) {
+  // console.log(request.user);
+  // in user you can access jwt after auth
+  // this can be useful to check roles/permissions per user
   const usersCursor = await this.mongo.db.collection('users').find({})
   const usersList = await usersCursor.toArray()
 
